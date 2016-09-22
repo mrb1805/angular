a66c45eec250b51764882cba1076c065bb291f83
mrb1805 <maahesh.rb@gmail.com>
2016-09-21 23:06:14 -0700

    Refactored Clean

--- EmployeeService.js
+++ EmployeeService.js
@@ -1,7 +1,7 @@
 function EmployeeService() {
-    var emp1 = { "fname": "apurva", "lname": "anand", "salary": 245 };
-    var emp2 = { "fname": "jamie", "lname": "dimon", "salary": 100000 };
-    var emp3 = { "fname": "morgan", "lname": "stanley", "salary": 12 };
+    var emp1 = { "fname": "ABC", "lname": "X", "salary": 245 };
+    var emp2 = { "fname": "DEF", "lname": "Y", "salary": 100000 };
+    var emp3 = { "fname": "GHI", "lname": "Z", "salary": 12 };
 
     var Employees = [emp1, emp2, emp3];
     return Employees;
