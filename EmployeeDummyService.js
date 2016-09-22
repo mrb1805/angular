3bf01222d301d7570d1641f4bdca370747fb460f
mrb1805 <maahesh.rb@gmail.com>
2016-09-22 03:18:16 -0700

    Service 1 Completed

--- EmployeeDummyService.js
+++ EmployeeDummyService.js
@@ -16,7 +16,15 @@ function EmployeeDummyService() {
     	Employees.push(e);
     	return Employees;
     }
-    myObject.Employees=Employees;
+    myObject.Employees=function(){
+		{
+    var emp1 = { "fname": "ABC", "lname": "X", "salary": 245 };
+    var emp2 = { "fname": "DEF", "lname": "Y", "salary": 100000 };
+    var emp3 = { "fname": "GHI", "lname": "Z", "salary": 12 };
+     var Employees = [emp1, emp2, emp3];
+     return Employees; //Call using EmployeeService.Employees();
+    };
+
     */
 
     return {
